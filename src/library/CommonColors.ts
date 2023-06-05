dbff62737959066c4bc4eb26c0a7a6ded61b871b
Quentin-rslt <quentinr82@protonmail.com>
2023-06-05 23:42:54 +0200

    Change colors

--- src/library/CommonColors.ts
+++ src/library/CommonColors.ts
@@ -1,9 +1,9 @@
 export enum COLORS {
     foreground = '#e8d0d0',
     foregroundHolder = '#827d7d',
-    componentBackground = "#352f2f",
-    background = '#1f1b1b',
-    backgroundLight = '#282323',
+    componentBackground = "#2f2a2a",
+    background = '#1d1818',
+    backgroundLight = '#261f1f',
     accentColor = '#c53e3ee5',
     clickableColor = '#c67979',
     transparent = 'transparent',
